6b3ae6e7b046862433694fc097b2e693bc23ac9e
Shruti Patil <41206697+shrutipatil1996@users.noreply.github.com>
2022-12-14 10:41:14 +0530

    Add files via upload
    Base Project

---
+++ Smack_App/Smack_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smack_App
+//
+//  Created by Shruti Patil on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
